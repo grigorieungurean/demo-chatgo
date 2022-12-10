928fc11a6c31f3b6ca570a665261e3edae1a9a8a
grigorieungurean <grigorie.ungurean.post@gmail.com>
2022-12-10 13:24:50 +0500

    Updates

---
+++ precache-manifest.c595645e0d5e6ce9579a8b2e6b7d2ba0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cba601df5817159dbd07b1151f98eb9",
+    "url": "/demo-chatgo/index.html"
+  },
+  {
+    "revision": "0818b375219345d6ea9e",
+    "url": "/demo-chatgo/static/css/3.77f50041.chunk.css"
+  },
+  {
+    "revision": "62ff14c7ef8070426530",
+    "url": "/demo-chatgo/static/css/4.a3d492d8.chunk.css"
+  },
+  {
+    "revision": "2eae54b74515743ed182",
+    "url": "/demo-chatgo/static/css/main.e120f713.chunk.css"
+  },
+  {
+    "revision": "d2d474ef34fbc1e19f76",
+    "url": "/demo-chatgo/static/js/2.02bcce4c.chunk.js"
+  },
+  {
+    "revision": "0818b375219345d6ea9e",
+    "url": "/demo-chatgo/static/js/3.78a29493.chunk.js"
+  },
+  {
+    "revision": "62ff14c7ef8070426530",
+    "url": "/demo-chatgo/static/js/4.b8b56ed5.chunk.js"
+  },
+  {
+    "revision": "2eae54b74515743ed182",
+    "url": "/demo-chatgo/static/js/main.96759138.chunk.js"
+  },
+  {
+    "revision": "713b58684c6b15b66be3",
+    "url": "/demo-chatgo/static/js/runtime~main.eed0408f.js"
+  },
+  {
+    "revision": "1895f619ba411eb9524eed4b33d33055",
+    "url": "/demo-chatgo/static/media/icons.1895f619.svg"
+  },
+  {
+    "revision": "c2574d71bee3a01c68d5673c6b721c9b",
+    "url": "/demo-chatgo/static/media/images-1.c2574d71.jpg"
+  },
+  {
+    "revision": "f3b918329e3cd2a87e915861fea80f72",
+    "url": "/demo-chatgo/static/media/images-2.f3b91832.jpg"
+  },
+  {
+    "revision": "b3ba703c591edd4aad57f8f4561a287b",
+    "url": "/demo-chatgo/static/media/montserrat-medium.b3ba703c.ttf"
+  },
+  {
+    "revision": "3fe868a1a9930b59d94d2c1d79461e3c",
+    "url": "/demo-chatgo/static/media/montserrat-regular.3fe868a1.ttf"
+  },
+  {
+    "revision": "fb428a00b04d4e93deb4d7180814848b",
+    "url": "/demo-chatgo/static/media/montserrat-semibold.fb428a00.ttf"
+  },
+  {
+    "revision": "dc04c36aa62a72eb1966770642ac1d38",
+    "url": "/demo-chatgo/static/media/preloader.dc04c36a.svg"
+  }
+]);
